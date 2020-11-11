df44b7703ba2893d8e066856dcee37cfad58a5fc
Noah Franco <noah@macbooks-mbp.lan>
2020-11-11 14:51:28 -0500

    updated movies table

--- migrations/20201111105722_movies.js
+++ migrations/20201111105722_movies.js
@@ -3,12 +3,12 @@ exports.up = function(knex) {
       tbl.increments();
         
       tbl.integer("release_year").notNullable();
-      tbl.string("title").notNullable();
-      tbl.string("origin").notNullable();
-      tbl.string("director");
-      tbl.string("cast");
-      tbl.string("wiki_page");
-      tbl.string("plot"); 
+      tbl.string("title", 255).notNullable();
+      tbl.string("origin", 255)
+      tbl.string("director", 255);
+      tbl.string("cast", 255);
+      tbl.string("wiki_page", 255);
+      tbl.string("plot", 255); 
     })
   };
   
