2c77ebaa3c1875f09c49f95d4a08b08d29e0f8db
Noah Franco <noah@macbooks-mbp.lan>
2020-11-11 11:14:57 -0500

    added data to migrations

--- migrations/20201111105722_movies.js
+++ migrations/20201111105722_movies.js
@@ -1,8 +1,17 @@
-
 exports.up = function(knex) {
+    return knex.schema.createTable("movies", tbl => {
+      tbl.increments();
+        
+      tbl.integer("release_year").notNullable();
+      tbl.string("title").notNullable();
+      tbl.string("origin").notNullable();
+      tbl.string("director");
+      tbl.string("cast");
+      tbl.string("wiki_page");
+      tbl.string("plot"); 
+    })
+  };
   
-};
-
-exports.down = function(knex) {
-  
-};
+  exports.down = function(knex) {
+    return knex.schema.dropTableIfExists("movies");
+  };
